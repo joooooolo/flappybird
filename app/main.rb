a859d65d9ee93c1ca76bb33de649bde2b8d2c4e4
joooooolo <jolocod@gmail.com>
2023-06-11 20:28:50 -0400

    Moving obstacles with a timer

--- app/main.rb
+++ app/main.rb
@@ -1,20 +1,20 @@
 class Flappybird
-
-  SPEED = 10
+  TIMER = 10
+  FPS = 60
+  SPEED = 2
 
   def initialize(args)
 
+    @args = args
     @obstacles = [
-      {x: 200, y: 450, w: 150, h: @args.grid.h, r: 0, g: 128, b: 0},
-      {x: 200, y: 0, w: 150, h: 250, r: 0, g: 128, b: 0}
-    ]
 
-    @args =args
+      {x: 1280, y: 450, w: 150, h: 650, r: 0, g: 128, b: 0},
+      {x: 1280, y: 0, w: 150, h: 250, r: 0, g: 128, b: 0},
+    ]
+    @timer = TIMER*FPS
 
     @player = {
-      x: 300,
-      y: 300,
-      w: 90,
+      x: 300,    y: 300,      w: 90,
       h: 75,
       path: "sprites/hexagon/orange.png",
     }
@@ -22,25 +22,33 @@ class Flappybird
 
   def tick
     render
+    spawn_obstacles
     moving_obstacles
   end
 
   def render
     @args.outputs.solids << {x: 0, y: 0, w: @args.grid.w, h: @args.grid.h, r: 135, g: 206,b:235}
-    # @args.outputs.solids << {x: 200 , y:0, w: 150, h: 250, r: 0, g: 128, b: 0}
-    # @args.outputs.solids << {x: 200, y: 450, w: 150, h: @args.grid.h, r: 0, g: 128, b: 0}
     @args.outputs.sprites << [@player, @obstacles]
   end
 
+  def spawn_obstacles
+@timer +=1
+if @timer%200==0
+    @obstacles << {x: 1280, y: 450, w: 150, h: 650, r: 0, g: 128, b:0}
+    @obstacles << {x: 1280, y: 0, w: 150, h: 250, r: 0, g:128, b: 0}
+end
+  end
+
   def moving_obstacles
+
     @obstacles.each do |obstacle|
-      obstacle[:x] -= SPEED
+     obstacle[:x] -= SPEED
+    end
     end
   end
-end
   def tick(args)
     args.state.game ||= Flappybird.new(args)
     args.state.game.tick
+   # args.state.moving_obstacles
   end
-
   $gtk.reset
